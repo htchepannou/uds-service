ec45a581d9fe275823dce6ea078c819f73b58e0b
htchepannou <htchepannou@expedia.com>
2015-07-13 17:10:17 -0400

    add installation scripts

---
+++ etc/install/initd.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Startup script for a spring boot project
+#
+# chkconfig: - 84 16
+# description: spring boot project
+
+USER_ID=uds
+SERVICE_NAME=uds
+ACTIVE_PROFILE=__ACTIVE_PROFILE__
+
+SERVICE_DIR=/opt/$SERVICE_NAME
+PATH_TO_JAR=$SERVICE_DIR/$SERVICE_NAME.jar
+PID_PATH_NAME=$SERVICE_DIR/$SERVICE_NAME.pid
+LOG_FILE=$SERVICE_DIR/log/$SERVICE_NAME.log
+
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            JAVA_OPTS="--Xms256m -Xmx256m --spring.profiles.active=$ACTIVE_PROFILE --logging.file=$LOG_FILE --spring.pidfile=$PID_PATH_NAME"
+            su - $USER_ID -c "java -jar $PATH_TO_JAR $JAVA_OPTS > /dev/null 2>&1  &"
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+
+    restart)
+        $0 stop
+        $0 start
+    ;;
+
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+esac
---
+++ etc/install/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Usage: ./install.sh <app-name> <profile>
+#
+
+if [ $# -ne 2 ];then
+    echo "Install failed! Illegal number of parameters"
+    echo "Usage: ./install.sh <spring-profile>"
+    exit 1
+fi
+
+PROFILE=$1
+
+# Install application
+if [ ! -d "/opt/$APP" ]; then
+  mkdir /opt/$APP
+fi
+
+if [ ! -d "/opt/$APP/log" ]; then
+  mkdir /opt/$APP/log
+fi
+
+chown -R $APP_USER:$APP_GROUP /opt/$APP
+
+
+# /etc/init.d
+cat initd.sh | sed -e "s/__ACTIVE_PROFILE__/$PROFILE/" > /etc/init.d/$APP
+
+chmod +x /etc/init.d/$APP
+
+/etc/init.d/$APP restart
