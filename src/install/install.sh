4d69c4573e6589003c31ff6f1f3ab6352031d804
htchepannou <htchepannou@expedia.com>
2015-07-23 22:01:51 -0400

    fix installation script

--- src/install/install.sh
+++ src/install/install.sh
@@ -4,25 +4,31 @@
 # Usage: ./install.sh
 #
 
-APP=uds-service
+SERVICE_PREFIX=uds
+SERVICE_NAME="$SERVICE_PREFIX-service"
+SERVICE_VERSION="1.0"
+SERVICE_PROFILE=test
+
+# Copy the jar from S3
+aws s3 cp s3://maven.com.tchepannou/release/com/tchepannou/$SERVICE_PREFIX/$SERVICE_NAME/$SERVICE_VERSION/$SERVICE_NAME-1.0-exec.jar $SERVICE_NAME-exec.jar
 
 # Install application
-if [ ! -d "/opt/$APP" ]; then
-  mkdir /opt/$APP
+if [ ! -d "/opt/$SERVICE_NAME" ]; then
+  mkdir /opt/$SERVICE_NAME
 fi
 
-if [ ! -d "/opt/$APP/log" ]; then
-  mkdir /opt/$APP/log
+if [ ! -d "/opt/$SERVICE_NAME/log" ]; then
+  mkdir /opt/$SERVICE_NAME/log
 fi
 
 # startup script
-cp initd.sh /etc/init.d/$APP
-chmod +x /etc/init.d/$APP
+cat initd.sh |  sed -e "s/__ACTIVE_PROFILE__/$SERVICE_PROFILE/" > /etc/init.d/initd.sh
+chmod +x /etc/init.d/$SERVICE_NAME
 
-/sbin/chkconfig --add $APP
-/sbin/chkconfig $APP on
+/sbin/chkconfig --add $SERVICE_NAME
+/sbin/chkconfig $SERVICE_NAME on
 
 # application
-cp $APP-exec.jar /opt/$APP/.
+cp $SERVICE_NAME-exec.jar /opt/$SERVICE_NAME/.
 
-/etc/init.d/$APP restart
+/etc/init.d/$SERVICE_NAME restart
